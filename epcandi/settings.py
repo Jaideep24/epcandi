52ddba17a7b27957cacebfdf406b281f8f3e2609
Jaideep <vinnijaideep@gmail.com>
2026-05-25 22:10:56 +0530

    feat: Update database settings to use dj_database_url for PostgreSQL and remove SQLite configuration; add new datadump.json file

--- epcandi/settings.py
+++ epcandi/settings.py
@@ -89,22 +89,15 @@ WSGI_APPLICATION = 'epcandi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
